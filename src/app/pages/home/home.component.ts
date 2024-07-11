c2f659257e7ddc16d15ca6d1bf7c6235c6985f8e
IgorDoring <idoring@tutanota.com>
2024-07-11 13:22:38 -0300

    feat: terminando retoques da primeira pagina, adicionando formulario e validacao

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  firstForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(3)]),
+  });
 
+  onSubmit() {
+    if(this.firstForm.valid){
+      alert(this.firstForm.value.email)
+    }
+  }
 }
