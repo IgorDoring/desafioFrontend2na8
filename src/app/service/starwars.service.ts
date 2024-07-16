2dfb6d7da33661115e5640285d209ff9816b4f89
IgorDoring <idoring@tutanota.com>
2024-07-16 15:31:51 -0300

    fix: refatorando service com novo modelo

--- src/app/service/starwars.service.ts
+++ src/app/service/starwars.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable, tap } from 'rxjs';
-import { People, PeopleResponse } from '../model/people';
+import { People, SearchResponse } from '../model/people';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StarwarsService {
-  readonly url = 'https://swapi.dev/api/people/?search=';
+  readonly url = 'https://swapi.dev/api/';
   http: HttpClient = inject(HttpClient);
 
   constructor() {}
 
-  getCharacter(char: string): Observable<PeopleResponse> {
-    return this.http.get<PeopleResponse>(this.url + char);
+  getCharacter(char: string): Observable<SearchResponse> {
+    return this.http.get<SearchResponse>(this.url + 'people/?search=' + char);
   }
 }
