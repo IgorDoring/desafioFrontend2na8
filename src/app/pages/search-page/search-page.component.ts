f849ac61f0529b63905e6646cf11066e8c408f57
IgorDoring <idoring@tutanota.com>
2024-07-19 10:28:31 -0300

    feat(search): salvando resultados na pagina

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -11,6 +11,7 @@ import { People, SearchResponse } from '../../model/people';
 import {
   concatMap,
   defaultIfEmpty,
+  endWith,
   finalize,
   forkJoin,
   from,
@@ -18,6 +19,7 @@ import {
   Observable,
   of,
   scan,
+  tap,
 } from 'rxjs';
 import { CardComponent } from '../../shared/card/card.component';
 import { LoadingComponent } from '../../shared/loading/loading.component';
@@ -29,85 +31,101 @@ import { LoadingComponent } from '../../shared/loading/loading.component';
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnInit{
   //todo pagina soh eh acessada ao digitar email
   searchForm = new FormGroup({
     search: new FormControl('', [Validators.required]),
   });
   isLoading: boolean = false;
-  erro: boolean = false;
+  hasError: boolean = false;
   search$: Observable<People | undefined> | undefined;
   resultado$: Observable<People> | undefined;
-  mensagem: string = '';
+  personagem: People | undefined
 
   constructor(private swService: StarwarsService) {}
 
+  ngOnInit(): void {
+    let personagem = localStorage.getItem('personagem')
+    console.log(personagem)
+    if(personagem != null){
+      this.search$ = of(JSON.parse(personagem))
+      this.resultado$ = of(JSON.parse(personagem))
+    }
+  }
+
   pesquisarNovamente() {
-    this.search$ = undefined
-    this.resultado$ = undefined
+    this.search$ = undefined;
+    this.resultado$ = undefined;
+    localStorage.removeItem('personagem')
   }
 
   onSubmit() {
     this.isLoading = true;
-    this.search$ = this.swService.getCharacter('Darth Vader').pipe(
-      map((res: SearchResponse) => {
-        console.log(res);
-        if (res.count != 1) {
-          this.erro = true;
-          this.searchForm.setValue({ search: '' });
-          this.isLoading = false;
-          return;
-        } else {
-          this.resultado$ = forkJoin({
-            name: of(res.results[0].name),
-            homeworld: of(res.results[0].homeworld).pipe(
-              concatMap((home) =>
-                this.swService.getHomeworld(home).pipe(map((home) => home.name))
+    this.search$ = this.swService
+      .getCharacter(this.searchForm.value.search!)
+      .pipe(
+        map((res: SearchResponse) => {
+          if (res.count != 1) {
+            this.hasError = true;
+            this.searchForm.setValue({ search: '' });
+            this.isLoading = false;
+            return;
+          } else {
+            this.resultado$ = forkJoin({
+              name: of(res.results[0].name),
+              homeworld: of(res.results[0].homeworld).pipe(
+                concatMap((home) =>
+                  this.swService
+                    .getHomeworld(home)
+                    .pipe(map((home) => home.name))
+                ),
+                defaultIfEmpty('No homeworld')
               ),
-              defaultIfEmpty('No homeworld')
-            ),
-            films: from(res.results[0].films).pipe(
-              concatMap((film) =>
-                this.swService.getFilm(film).pipe(map((film) => [film.title]))
+              films: from(res.results[0].films).pipe(
+                concatMap((film) =>
+                  this.swService.getFilm(film).pipe(map((film) => [film.title]))
+                ),
+                scan((all, curr) => [...all, ...curr]),
+                defaultIfEmpty([])
               ),
-              scan((all, curr) => [...all, ...curr]),
-              defaultIfEmpty([])
-            ),
-            starships: from(res.results[0].starships).pipe(
-              concatMap((starship) =>
-                this.swService
-                  .getStarship(starship)
-                  .pipe(map((starship) => [starship.name]))
+              starships: from(res.results[0].starships).pipe(
+                concatMap((starship) =>
+                  this.swService
+                    .getStarship(starship)
+                    .pipe(map((starship) => [starship.name]))
+                ),
+                scan((all, curr) => [...all, ...curr]),
+                defaultIfEmpty([])
               ),
-              scan((all, curr) => [...all, ...curr]),
-              defaultIfEmpty([])
-            ),
-            vehicles: from(res.results[0].vehicles).pipe(
-              concatMap((vehicle) =>
-                this.swService
-                  .getVehicle(vehicle)
-                  .pipe(map((vehicle) => [vehicle.name]))
+              vehicles: from(res.results[0].vehicles).pipe(
+                concatMap((vehicle) =>
+                  this.swService
+                    .getVehicle(vehicle)
+                    .pipe(map((vehicle) => [vehicle.name]))
+                ),
+                scan((all, curr) => [...all, ...curr]),
+                defaultIfEmpty([])
               ),
-              scan((all, curr) => [...all, ...curr]),
-              defaultIfEmpty([])
-            ),
-            species: from(res.results[0].species).pipe(
-              concatMap((specie) =>
-                this.swService
-                  .getSpecie(specie)
-                  .pipe(map((specie) => [specie.name]))
+              species: from(res.results[0].species).pipe(
+                concatMap((specie) =>
+                  this.swService
+                    .getSpecie(specie)
+                    .pipe(map((specie) => [specie.name]))
+                ),
+                scan((all, curr) => [...all, ...curr]),
+                defaultIfEmpty([])
               ),
-              scan((all, curr) => [...all, ...curr]),
-              defaultIfEmpty([])
-            ),
-          })
-          .pipe(
-            finalize(() => {console.log("end");this.isLoading = false})
-          );
+            }).pipe(
+              tap( personagem => localStorage.setItem('personagem', JSON.stringify(personagem))),
+              finalize(() => {
+                this.isLoading = false;
+                this.hasError = false
+              })
+            );
 
-          return res.results[0]!;
-        }
-      })
-    );
+            return res.results[0]!;
+          }
+        })
+      );
   }
 }
