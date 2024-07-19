9020830b42f43d90ab7eb5d805fadf80b72a6081
IgorDoring <idoring@tutanota.com>
2024-07-18 21:37:07 -0300

    feat(searchpage): consumindo dados da API

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -8,7 +8,17 @@ import {
 } from '@angular/forms';
 import { StarwarsService } from '../../service/starwars.service';
 import { People, SearchResponse } from '../../model/people';
-import { map, Observable, tap } from 'rxjs';
+import {
+  concatMap,
+  defaultIfEmpty,
+  finalize,
+  forkJoin,
+  from,
+  map,
+  Observable,
+  of,
+  scan,
+} from 'rxjs';
 import { CardComponent } from '../../shared/card/card.component';
 import { LoadingComponent } from '../../shared/loading/loading.component';
 
@@ -26,34 +36,78 @@ export class SearchPageComponent {
   });
   isLoading: boolean = false;
   erro: boolean = false;
-  // search$: Observable<SearchResponse> | undefined;
+  search$: Observable<People | undefined> | undefined;
+  resultado$: Observable<People> | undefined;
   mensagem: string = '';
-
-  personagemMock: People | undefined;
 
   constructor(private swService: StarwarsService) {}
 
   pesquisarNovamente() {
-    this.personagemMock = undefined
+    this.search$ = undefined
+    this.resultado$ = undefined
   }
 
   onSubmit() {
     this.isLoading = true;
-    setTimeout(() => {
-      this.personagemMock = {
-        name: 'Darth Vader',
-        films: [
-          'A Ameaça Fantasma',
-          'Ataque dos Clones',
-          'A Vingança dos Sith',
-          'O Retorno de Jedi',
-        ],
-        starships: ['TIE Advanced'],
-        vehicles: [],
-        species: [],
-        homeworld: 'Tatooine',
-      };
-      this.isLoading = false;
-    }, 2000);
+    this.search$ = this.swService.getCharacter('Darth Vader').pipe(
+      map((res: SearchResponse) => {
+        console.log(res);
+        if (res.count != 1) {
+          this.erro = true;
+          this.searchForm.setValue({ search: '' });
+          this.isLoading = false;
+          return;
+        } else {
+          this.resultado$ = forkJoin({
+            name: of(res.results[0].name),
+            homeworld: of(res.results[0].homeworld).pipe(
+              concatMap((home) =>
+                this.swService.getHomeworld(home).pipe(map((home) => home.name))
+              ),
+              defaultIfEmpty('No homeworld')
+            ),
+            films: from(res.results[0].films).pipe(
+              concatMap((film) =>
+                this.swService.getFilm(film).pipe(map((film) => [film.title]))
+              ),
+              scan((all, curr) => [...all, ...curr]),
+              defaultIfEmpty([])
+            ),
+            starships: from(res.results[0].starships).pipe(
+              concatMap((starship) =>
+                this.swService
+                  .getStarship(starship)
+                  .pipe(map((starship) => [starship.name]))
+              ),
+              scan((all, curr) => [...all, ...curr]),
+              defaultIfEmpty([])
+            ),
+            vehicles: from(res.results[0].vehicles).pipe(
+              concatMap((vehicle) =>
+                this.swService
+                  .getVehicle(vehicle)
+                  .pipe(map((vehicle) => [vehicle.name]))
+              ),
+              scan((all, curr) => [...all, ...curr]),
+              defaultIfEmpty([])
+            ),
+            species: from(res.results[0].species).pipe(
+              concatMap((specie) =>
+                this.swService
+                  .getSpecie(specie)
+                  .pipe(map((specie) => [specie.name]))
+              ),
+              scan((all, curr) => [...all, ...curr]),
+              defaultIfEmpty([])
+            ),
+          })
+          .pipe(
+            finalize(() => {console.log("end");this.isLoading = false})
+          );
+
+          return res.results[0]!;
+        }
+      })
+    );
   }
 }
